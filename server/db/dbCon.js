4c2b16a957c46263b68d1e76a3ba38d4d6f475bd
Muhammad Naseem <muhammadnaseem0342@gmail.com>
2024-03-20 15:11:25 -0700

    Update dbCon.js

--- server/db/dbCon.js
+++ server/db/dbCon.js
@@ -1,15 +1,16 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "crm",
-})
+    host: "sql6.freesqldatabase.com",
+    user: "sql6692989",
+    password: "eFWhpcnxLC",
+    database: "sql6692989",
+    port: 3306
+});
 
 con.connect(function(error){
     if(error) throw error;
     console.log("Connection Successful");
-})
+});
 
 module.exports = con;
